6b98b25720827982b20b96446991a7e30d16c531
Axess <axess.kun@gmail.com>
2025-08-25 02:00:28 +0900

    change base to './' to fix relative path error

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? 'test-gh-pages' : './',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true, // also necessary
