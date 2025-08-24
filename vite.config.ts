a3cbf7cbae5be4b8787d70fc3b56f041038b3e20
Axess <axess.kun@gmail.com>
2025-08-25 01:43:51 +0900

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? 'test-gh-pages' : './',
   build: {
     outDir: 'docs',
     emptyOutDir: true, // also necessary
